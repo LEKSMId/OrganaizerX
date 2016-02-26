0489a432af6e4c51bb5fd31c126f5ec9fc3345c1
Alex <a.prenko@gmail.com>
2016-02-26 12:08:15 +0200

    Rebuild create and delete event

--- OrganaizerX/OrganaizerX/mainViewController.swift
+++ OrganaizerX/OrganaizerX/mainViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import CoreData
+import EventKit
 
 class mainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
     var items : [Item] = []
+    
     
     @IBOutlet weak var pickerDate: UIDatePicker!
     @IBOutlet weak var pickerDateTime: UIDatePicker!
@@ -131,6 +133,84 @@ class mainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @IBAction func saveEvent(sender: AnyObject) {
         setDateAndTime()
-//        saveNewItem()
+        
+        let eventStore = EKEventStore()
+        
+        let startDate = NSDate()
+        
+        let endDate = startDate.dateByAddingTimeInterval(60 * 60) // One hour
+        
+        if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
+            eventStore.requestAccessToEntityType(.Event, completion: {
+                granted, error in
+                self.createEvent(eventStore, title: "DJ's Test Event", startDate: startDate, endDate: endDate)
+            })
+        } else {
+            createEvent(eventStore, title: "DJ's Test Event", startDate: startDate, endDate: endDate)
+        }
+        
+        //saveNewItem()
+    }
+    
+    var savedEventId : String = ""
+    
+    func createEvent(eventStore: EKEventStore, title: String, startDate: NSDate, endDate: NSDate) {
+        let event = EKEvent(eventStore: eventStore)
+        
+        event.title = title
+        event.startDate = startDate
+        event.endDate = endDate
+        event.calendar = eventStore.defaultCalendarForNewEvents
+        do {
+            try eventStore.saveEvent(event, span: .ThisEvent)
+            savedEventId = event.eventIdentifier
+        } catch {
+            print("Bad things happened")
+        }
+    }
+    
+    // Removes an event from the EKEventStore. The method assumes the eventStore is created and
+    // accessible
+    
+    func deleteEvent(eventStore: EKEventStore, eventIdentifier: String) {
+        let eventToRemove = eventStore.eventWithIdentifier(eventIdentifier)
+        if (eventToRemove != nil) {
+            do {
+                try eventStore.removeEvent(eventToRemove!, span: .ThisEvent)
+            } catch {
+                print("Bad things happened")
+            }
+        }
+    }
+    
+//    @IBAction func addEvent(sender: UIButton) {
+//        let eventStore = EKEventStore()
+//    
+//        let startDate = NSDate()
+//        let endDate = startDate.dateByAddingTimeInterval(60 * 60) // One hour
+//    
+//        if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
+//            eventStore.requestAccessToEntityType(.Event, completion: {
+//                granted, error in
+//                self.createEvent(eventStore, title: "DJ's Test Event", startDate: startDate, endDate: endDate)
+//            })
+//        } else {
+//            createEvent(eventStore, title: "DJ's Test Event", startDate: startDate, endDate: endDate)
+//        }
+//    }
+    
+    
+    // Responds to button to remove event. This checks that we have permission first, before removing the
+    // event
+    @IBAction func removeEvent(sender: UIButton) {
+        let eventStore = EKEventStore()
+    
+        if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
+            eventStore.requestAccessToEntityType(.Event, completion: { (granted, error) -> Void in
+                self.deleteEvent(eventStore, eventIdentifier: self.savedEventId)
+            })
+        } else {
+            deleteEvent(eventStore, eventIdentifier: savedEventId)
+        }
     }
 }
