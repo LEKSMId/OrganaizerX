fbc03794f7fc62d646e0a8a068b171ff674496a9
Alex <a.prenko@gmail.com>
2016-03-03 16:25:17 +0200

    Добавлено поле поиска, доработано обновление таблици с ивентами

--- OrganaizerX/OrganaizerX/mainViewController.swift
+++ OrganaizerX/OrganaizerX/mainViewController.swift
@@ -10,47 +10,80 @@ import UIKit
 import EventKit
 import CoreData
 
-class mainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class mainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var items : [Item] = []
+    var filteredItems = [Item]()
     var refreshControl: UIRefreshControl!
+    var inSearchMode = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        self.searchBar.delegate = self
+        
             
-            let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
-            
-            let request = NSFetchRequest(entityName: "Item")
-            var result : [AnyObject]?
-            
-            do {
-                result = try context.executeFetchRequest(request)
-            } catch _ {
-                result = nil
-            }
-            
-            if result != nil {
-                self.items = result as! [Item]
-            }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        updateUI()
+    }
+    
+    func updateUI() {
+        let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+        
+        let request = NSFetchRequest(entityName: "Item")
+        var result : [AnyObject]?
+        
+        do {
+            result = try context.executeFetchRequest(request)
+        } catch _ {
+            result = nil
+        }
+        
+        if result != nil {
+            self.items = result as! [Item]
+        }
+        self.tableView.reloadData()
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //        return 5
-          return self.items.count
+        
+        if inSearchMode{
+            return filteredItems.count
+        } else {
+            return self.items.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
         if let call = tableView.dequeueReusableCellWithIdentifier("eventCell") as? eventCell{
             
-            let item = self.items[indexPath.row]
+            let item: Item!
+//            let item = self.items[indexPath.row]
+            
+            if inSearchMode {
+              item = filteredItems[indexPath.row]
+            } else {
+                item = self.items[indexPath.row]
+            }
+            
+            
+            if inSearchMode {
+                
+            } else {
+                
+            }
             
             var imag : UIImage!
             
@@ -65,14 +98,24 @@ class mainViewController: UIViewController, UITableViewDelegate, UITableViewData
             call.configureCell(imag, text: item.title!, note: item.noteEvent!)
             return call
 
-        } else {
+        } else {    
             return eventCell()
         
         }
     }
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar == "" {
+            inSearchMode = false
+        
+        } else {
+        
+            inSearchMode = true
+            let lower = searchBar.text!.lowercaseString
+            filteredItems = items.filter({$0.title?.rangeOfString(lower) != nil})
+            tableView.reloadData()
     
-    func reloadData() {
-        self.tableView.reloadData()
+        }
     }
     
 }
